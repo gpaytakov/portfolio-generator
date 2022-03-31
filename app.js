810201792b76b9fb3940f5dda4cbadbe9fb41475
gpay <gpaytakov@gmail.com>
2022-03-30 20:28:10 -0500

    inquirer asks question and records answer in key:value format

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
-// importing fs=fileSystem module
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// importing inquirer package
+const inquirer = require('inquirer');
+// // importing fs=fileSystem module
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-// const profileDataArgs = process.argv.slice(2, process.argv.lenght);
+// const pageHTML = generatePage(name1, github);
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-// // template literals
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-
-const profileDataArgs = process.argv.slice(2);
-// // long variable assignment method
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-
-// short variable assignment method
-const [name1, github] = profileDataArgs;
-
-// // this is a test, if it is working 
-// const generatePage = (userName, githubName) => {
-//   return `
-//     Name: ${userName}
-//     GitHub: ${githubName}
-//   `;
-// };
-
-fs.writeFile('./index.html', generatePage(name1, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
